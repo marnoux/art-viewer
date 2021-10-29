c7e00fabfc53ef0f07e5f26e988b7bf7c1d7fa0b
Marnoux <marnouxmanser@gmail.com>
2021-10-29 13:26:50 +0200

    refactor to only include single artist

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-
 import MainHeader from './components/UI/MainHeader/MainHeader';
 import Paintings from './components/Paintings/Paintings';
 
 const App = () => {
-	const baseUrlString =
-		'https://www.rijksmuseum.nl/api/nl/collection?key=yW6uq3BV&involvedMaker=';
-	const [artist, setArtist] = useState('');
-	const [artObjects, setArtObjects] = useState([]);
-	const [baseURL, setBaseURL] = useState(baseUrlString);
-
-	const artistChangeHandler = (artist) => {
-		setArtist(artist);
-		setBaseURL(baseUrlString + artist.replace(' ', '+').trim());
-
-		if (artist.length > 0) {
-			axios.get(baseURL).then((response) => {
-				setArtObjects(response.data);
-			});
-		}
-	};
-
 	return (
 		<div className='container'>
-			<MainHeader onChangeArtist={artistChangeHandler} />
-			<Paintings artObjects={artObjects} />
+			<MainHeader />
+			<Paintings />
 		</div>
 	);
 };
--- src/components/UI/MainHeader/MainHeader.js
+++ src/components/UI/MainHeader/MainHeader.js
@@ -1,34 +1,9 @@
-import React, { createRef } from 'react';
-import './MainHeader.css';
-
-const MainHeader = (props) => {
-	// Use ref to get input value
-	let textInput = createRef();
-
-	const artistInputChangeHandler = (e) => {
-		e.preventDefault();
-		props.onChangeArtist(textInput.current.value);
-	};
-
+const MainHeader = () => {
 	return (
 		<nav className='navbar navbar-dark mt-3'>
-			<h1 className='navbar-brand'>Online Art Gallery</h1>
-			<form className='d-flex'>
-				<input
-					className='form-control me-3'
-					ref={textInput}
-					type='search'
-					placeholder='Enter Name of Artist'
-				></input>
-				<button
-					className='btn btn-outline-success'
-					type='submit'
-					onClick={artistInputChangeHandler}
-				>
-					Search
-				</button>
-			</form>
-		</nav>
+			<h1>Het Rijksmuseum</h1>
+			<h2 className='navbar-brand'>Works by Vincent van Gogh</h2>
+		</nav> 
 	);
 };
 
